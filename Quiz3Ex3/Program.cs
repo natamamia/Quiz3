db35d41312ec1c38548dfad5c6699e4d9f19c759
natamamia <natamamia@gmail.com>
2022-11-15 17:15:16 +0400

    Add files via upload

--- Quiz3Ex3/Program.cs
+++ Quiz3Ex3/Program.cs
@@ -1,3 +1,26 @@
 ﻿// 3. ემოდის გამოსახულება სტრინგის სახით. 
 // დაწერეთ პროგრამა რომელიც ამ გამოსახულების შედეგს გამოთვლის (არითმეტიკული ოპერაციები ჩავთვალოთ რომ გვაქვს მხოლოდ + - * ) 
 // მაგ: “((1+2)*(3+4))*(5*(8+9))”
+
+
+Console.WriteLine("Stack");
+Stack<string> stack = new Stack<string>();
+
+stack.Push("(");
+stack.Push(")");
+stack.Push("+");
+stack.Push("-");
+stack.Push("*");
+
+stack.Push("1");
+stack.Push("2");
+stack.Push("3"); 
+stack.Push("4");
+stack.Push("5");
+stack.Push("8");
+stack.Push("9");
+
+while(stack.Count != 0)
+{
+
+}
